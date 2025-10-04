ae748e065b82c03f694d3581eec188fdd86189f9
yan <yanyam.xue@gmail.com>
2025-10-04 14:25:17 -0700

    test

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -10,7 +10,7 @@ export function HapticTab(props: BottomTabBarButtonProps) {
         if (process.env.EXPO_OS === 'ios') {
           // Add a soft haptic feedback when pressing down on the tabs.
           Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        }
+        } //dwajdwa 
         props.onPressIn?.(ev);
       }}
     />
