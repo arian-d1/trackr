c97d0369fc7ee9f45b0c6c193171c8febc085a16
ihawp <ihawpebay@gmail.com>
2025-10-05 04:20:58 -0700

    added schemas for users, friennds, captures

---
+++ backend/models/Friends.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+/*
+const friendSchema = new mongoose.Schema({
+  id: Number,
+  friend_id_1: Number,
+  friend_id_2: Number
+});
+*/
+
+const friendSchema = new mongoose.Schema({
+  friend_id_1: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  friend_id_2: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+const Friend = mongoose.model('Friend', friendSchema);
+
+export default Friend;
